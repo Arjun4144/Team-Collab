ae776da020d8c7e39c5b46a954e145195cd0a75b
Arjun Rawat <arjunrawat4144@gmail.com>
2026-05-01 12:58:59 +0530

    feat: implement WebRTC voice and video calling infrastructure with real-time signaling

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://team-collab-ntlm.onrender.com/api',
+  baseURL: 'https://team-collab-1-64qq.onrender.com/api',
   headers: { 'Content-Type': 'application/json' }
 });
 
